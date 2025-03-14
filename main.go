19eb93631f578f1897cf4794ea7ae4307f392c41
Sarthak Shrivastava <161567581+SarthakShrivastav-a@users.noreply.github.com>
2025-03-14 17:01:12 +0530

    Mapped paths with Functions-Completed logic for Adding Monitor in db

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"Uptime/config"
+	"Uptime/handlers"
+	"log"
+	"net/http"
+)
+
+func main() {
+	db, err := config.InitDB()
+	if err != nil {
+		log.Fatal("Failed to connect to database")
+	}
+	defer db.Close()
+
+	http.HandleFunc("/add_monitor", handlers.AddMonitorHandler(db))
+	http.HandleFunc("/get_monitors", handlers.GetAllMonitorsHandler(db))
+
+	log.Println("Server running on port 8081")
+	http.ListenAndServe(":8081", nil)
+}
